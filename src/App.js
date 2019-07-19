e6f12228e11e84db0e908c7ad1ab29550966f376
Albert Chung <chung972@gmail.com>
2019-07-19 13:42:55 -0500

    git status imports and renders UsernameForm component; adds onUsernameSubmitted()

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react'
+import UsernameForm from "./components/UsernameForm";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state={
+      currentUsername:"",
+    }
+    this.onUsernameSubmitted = this.onUsernameSubmitted.bind(this);
+  }
+
+  onUsernameSubmitted(username){
+    fetch("http://localhost:3001/users", {
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({username})
+    })
+    .then(response => {
+      this.setState({
+        currentUsername: username
+      })
+    })
+    .catch(error=>{
+      console.error("error", error);
+    })
+  }
+
+
   render() {
-    return <h1>Chatly</h1>
+    return <UsernameForm onSubmit={this.onUsernameSubmitted} />
   }
 }
 
